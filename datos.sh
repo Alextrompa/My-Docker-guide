5bda2017430fe828f73e0553dcb05dc2b53b09c9
Alejandro <alexiando29@gmail.com>
2018-03-26 21:13:56 +0200

    Conceptos y run
    Explicacion de los conceptos de Imagen y Contenedor y como usar el comando run. run todavia sin acciones

--- datos.sh
+++ datos.sh
@@ -5,7 +5,6 @@
 	# Si esto no nos gusta y queremos desactivar esto
 	
 	sudo systemctl disable docker
-	
 
 
 #Existen dos versiones de Docker
@@ -16,3 +15,22 @@
 #Imagenes de las distintas aplicaciones
 #	https://store.docker.com/search?source=verified&type=image
 
+
+# Dos conceptos, Imagen y Contenedor
+
+#	Imagen:		Instrucciones para crear un contenedor. Estas son solo de lectura.
+#	Contenedor:	Es una instancia ejecutable de una imagen. Se pueden crear, poner 
+#			en marcha, parar, mover o borrar. Incluso se puede crear una 
+#			Imagen a partir del estado actual de un contenedor.
+
+
+# Para ejecutar cualquier imagen se utiliza el comando "docker run Imagen"
+# Este comando comprueba las librerias que utiliza la Imagen. En caso de que falte alguna, la descargará del repositorio (como si utilizara docker pull)
+# Ejemplos
+	docker run hello-world		# Esta Imagen suele ejecutarse para comprpobar que la instalanción del programa funciona correctamente.
+	docker run -it ubuntu bash	# Esta Imagen es el bash de ubuntu. Es como ejecutar el mismo programa en la susodicha distribución.
+	docker run openjdk		# Sí, el kit de desarrollo de Java. Se utiliza para hacer contenedores de aplicaciones Java. (También existen para Haskell y Ruby entre otros)
+
+# Opciones
+
+	
