6becc94116ebe47dea06cfc871164a3d4d9c0402
root <root@localhost.localdomain>
2018-06-25 11:25:19 +0200

    Añadidos los comandos de instalación de Docker Compose y Docker Machine

--- docker.sh
+++ docker.sh
@@ -23,6 +23,21 @@ dnf config-manager \
     
 dnf install docker-ce
 
+#Vamos a instalar docker compose, una herramienta de Docker
+curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+
+chmod +x /usr/local/bin/docker-compose
+
+docker-compose --version
+
+#Instalamos docker-machine, otra herramienta
+
+base=https://github.com/docker/machine/releases/download/v0.14.0 &&
+  curl -L $base/docker-machine-$(uname -s)-$(uname -m) >/tmp/docker-machine &&
+  sudo install /tmp/docker-machine /usr/local/bin/docker-machine
+
+docker-machine version
+
 #Hacemos que se inicie siempre al arrancar la máquina
 systemctl enable docker
 
